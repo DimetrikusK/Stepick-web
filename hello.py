61a8a9be71c6e64fb88cc2167b240a965a1d2e44
root <root@dk.dk>
2020-12-26 17:55:18 +0300

    maybe

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
-def wsgi_application(environ, start_response):
-    print(environ)
-    # status = '200 OK'
-    # headers = [
-    #     ('Content-Type', 'text/plain')
-    # ]
-    # body = 'Hello, world!'
-    # start_response(status, headers)
-    # return [body]
+def app(environ, start_response):
+     status = '200 OK'
+     headers = [
+         ('Content-Type', 'text/plain')
+          ]
+     body = [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding='utf8')]
+     start_response(status, headers)
+     return body
--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
-#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo /etc/init.d/nginx restart
+#ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#/etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+/etc/init.d/nginx restart
+gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf hello:app
+gunicorn -c /home/box/web/etc/gunicorn-django.conf asl.wsgi:aplication
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
-sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/gunicorn restart
