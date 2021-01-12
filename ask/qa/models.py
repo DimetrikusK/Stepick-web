61deba8e0b6e4c762cd0016e8adf6e37e914a6f3
dimetrikusk <dimetrikusk@gmail.com>
2021-01-12 12:29:58 +0300

    db

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -19,7 +19,7 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
-    rating = models.ImageField(blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes')
 
