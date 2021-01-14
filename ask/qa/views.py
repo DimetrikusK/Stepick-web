4ad6ac8a625fb17365c1478105603a873d7201f6
dimetrikusk <dimetrikusk@gmail.com>
2021-01-14 09:41:58 +0300

    views

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .utils import paginate
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_GET
@@ -13,32 +12,63 @@ from django.contrib.auth import authenticate, login, logout
 
 @require_GET
 def main_page(request, *args, **kwargs):
-    last_questions = Question.objects.new()
-    page = paginate(request, last_questions)
-    page.base_url = '/?page='
-    return render(request, 'qa/index.html', {
-        'questions': page.object_list,
-        'page': page,
-    })
+    question_list = Question.objects.order_by('-id')
+    paginator, page, limit = paginate(request, question_list)
+    context = {
+        'questions': page,
+        'paginator': paginator,
+        'limit': limit,
+    }
+    return render(request, 'qa/index.html', context)
 
 
 def test(request):
     return HttpResponse(status=200)
 
 
-def question(request):
-    q = get_object_or_404(Question, id=id)
+def question(request, question_id):
+    q = get_object_or_404(Question, id=question_id)
     a = q.answer_set.all()
     # a = Answer.objects.filter(question=question_id).order_by('-added_at')
+    # form = AnswerForm(initial={'question': question_id})
     context = {'question': q, 'answers': a}
     return render(request, 'qa/question.html', context)
 
 
 def popular(request):
-    popular_questions = Question.objects.popular()
-    page = paginate(request, popular_questions)
-    page.base_url = '/popular/?page='
-    return render(request, 'qa/index.html', {
-        'questions': page.object_list,
-        'page': page
-    })
+    question_list = Question.objects.order_by('-rating')
+    paginator, page, limit = paginate(request, question_list)
+    context = {
+        'questions': page,
+        'paginator': paginator,
+        'limit': limit,
+    }
+    return render(request, 'qa/popular.html', context)
+
+
+
+def paginate(request, lst):
+    # get limit
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+
+    # if limit is too high, normalize it
+    if limit > 100:
+        limit = 10
+
+    paginator = Paginator(lst, limit)
+
+    # get current page
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+
+    return paginator, page, limit
